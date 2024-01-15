4ab80715a1345c73c0e6bf92f9addcd59e43a7b0
Wahyuadin.zip <mohammadwahyuadinugroho@gmail.com>
2024-01-15 21:58:02 +0700

    update 3

--- Controller/RiwayatController.php
+++ Controller/RiwayatController.php
@@ -18,4 +18,29 @@ function saldoAdmin() {
     return mysqli_fetch_object(mysqli_query($conn, "SELECT SUM(nominal) as SALDO FROM riwayat_transaksi"));
 }
 
+function acc($data) {
+    $id = htmlspecialchars($data['id_transaksi']);
+    $conn = globalfun();
+    $result = mysqli_fetch_object(mysqli_query($conn, "SELECT * FROM riwayat_transaksi WHERE id_transaksi='$id'"));
+    $array = [
+        'id_user'           =>  $result->id_user,
+        'id_transaksi'      =>  $result->id_transaksi,
+        'nama'              =>  mysqli_fetch_object(mysqli_query($conn, "SELECT users.nama
+                                FROM users INNER JOIN riwayat_transaksi ON 
+                                riwayat_transaksi.id_user = users.id_user WHERE id_transaksi='$id'"))->nama,
+        'nama_transaksi'    =>  $result->kategori,
+        'nominal'           =>  $result->nominal,
+        'ket'               =>  $result->ket
+    ];
+    $object = (object)$array;
+    $result1 = mysqli_query($conn, "INSERT INTO pemasukan (id_user,id_transaksi,nama,nama_transaksi,nominal,ket) VALUES ('$object->id_user','$object->id_transaksi','$object->nama','$object->nama_transaksi','$object->nominal','$object->ket')");
+    if ($result1) {
+        mysqli_query($conn, "UPDATE riwayat_transaksi SET acc='1' WHERE id_transaksi='$object->id_transaksi'");
+        return mysqli_query($conn, "INSERT INTO master (id_user,id_transaksi,nama,pemasukan,ket) VALUES ('$object->id_user','$object->id_transaksi','$object->nama','$object->nominal','$object->ket')");
+    }
+}
+
+function reject() {
+    
+}
 ?>
--- dashboard/pengajuan-pemasukan.php
+++ dashboard/pengajuan-pemasukan.php
@@ -3,29 +3,17 @@ session_start();
 $judul = 'Pengajuan Pemasukan';
 include(__DIR__.'/../template/header.php');
 if (!isset($_SESSION['users'])) {
-    return header('location:'. base_url());
+	return header('location:'.base_url());
 }
 include(__DIR__.'/../template/navbar_header.php');
 include(__DIR__.'/../template/sitebar.php');
 
-include(__DIR__.'./../Controller/PemasukanController.php');
+include(__DIR__.'/../Controller/RiwayatController.php');
 
-if (isset($_POST['tambah'])) {
-    if (adminInsert($_POST)) {
+if (isset($_POST['acc'])) {
+    if (acc($_POST)) {
         set_flash_data('berhasil', 'Data Berhasil Disimpan!');
-    }
-}
-
-if (isset($_POST['hapus'])) {
-    if (adminHapus($_POST)) {
-        set_flash_data('berhasil', 'Data Berhasil Dihapus!');
-    }
-}
-
-if (isset($_POST['edit'])) {
-    if (adminEdit($_POST)) {
-        set_flash_data('berhasil', 'Data Berhasil Diedit!');
-    }
+    }  
 }
 ?>
 <div class="container">
@@ -38,38 +26,38 @@ if (isset($_POST['edit'])) {
 									<i class="flaticon-home"></i>
 								</a>
 							</li>
+                            <li class="separator">
+								<i class="flaticon-right-arrow"></i>
+							</li>
+                            <li>
+                                <a>Pemasukan Report</a>
+                            </li>
 							<li class="separator">
 								<i class="flaticon-right-arrow"></i>
 							</li>
 							<li class="nav-item">
-								<a>Pemasukan Report</a>
-							</li>
-              <li class="separator">
-								<i class="flaticon-right-arrow"></i>
-							</li>
-              <li class="nav-item">
-								<a href="<?= base_url('dashboard/pemasukan.php')?>"><?= $judul?></a>
+								<a href="<?= base_url('dashboard/pengajuan-pemasukan.php')?>"><?= $judul?></a>
 							</li>
 						</ul>
 					</div>
 					<div class="row">
 						<div class="col-md-12">
 							<div class="card">
-                <div class="card-header">
-                  <?php
-                  $gagal = get_flash_data('gagal');
-                  if ($gagal) { ?>
-                  <div class="alert alert-danger" role="alert">
-                      <?= $gagal;?>
-                  </div>
-                  <?php } ?>
-                  <?php
-                  $berhasil = get_flash_data('berhasil');
-                  if ($berhasil) { ?>
-                  <div class="alert alert-success" role="alert">
-                      <?= $berhasil;?>
-                  </div>
-                  <?php } ?>
+                                <div class="card-header">
+                                <?php
+                                $gagal = get_flash_data('gagal');
+                                if ($gagal) { ?>
+                                <div class="alert alert-danger" role="alert">
+                                    <?= $gagal;?>
+                                </div>
+                                <?php } ?>
+                                <?php
+                                $berhasil = get_flash_data('berhasil');
+                                if ($berhasil) { ?>
+                                <div class="alert alert-success" role="alert">
+                                    <?= $berhasil;?>
+                                </div>
+                                <?php } ?>
 								</div>
 								<div class="card-body">
 									<div class="table-responsive">
@@ -77,93 +65,89 @@ if (isset($_POST['edit'])) {
 											<thead>
 												<tr>
 													<th>No</th>
-													<th>Nama Nasabah</th>
+													<th>Nama</th>
+													<th>Kategori Transaksi</th>
 													<th>Nominal</th>
 													<th>Keterangan</th>
 													<th>Waktu</th>
 													<th>Action</th>
 												</tr>
 											</thead>
+                                            <?php $no =1;?>
 											<tbody>
-                          <?php $no=1; foreach (adminData() as $data) { ?>
-                          <tr>
+                                                <?php $conn = globalfun();
+												foreach (mysqli_fetch_all(mysqli_query($conn,
+												"SELECT riwayat_transaksi.id_transaksi, users.nama, riwayat_transaksi.kategori, riwayat_transaksi.nominal, riwayat_transaksi.ket, riwayat_transaksi.timestamp
+                                                FROM users
+                                                INNER JOIN riwayat_transaksi ON riwayat_transaksi.id_user = users.id_user
+                                                WHERE riwayat_transaksi.acc = '0'")) as $data) { ?>
+                                                <tr>
+                                                    <!-- <?php var_dump($data)?> -->
 													<td><?= $no++?></td>
-													<td><?= $data[3]?></td>
-													<td><?= rupiah($data[5])?></td>
-													<td><?= $data[6];?></td>
-													<td><?= $data[7];?></td>
-													<td>
-                              <a style="color: white;" class="btn btn-success btn-sm" data-toggle="modal" data-target="#edit<?=$data['2']?>"><i class="fas fa-info-circle" style="margin-right: 10px;"></i>Edit</a>
-                              <button type="button" href="<?= base_url('dashboard/hapus-pemasukan.php?detail=').$data['2']?>" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#hapus<?=$data['2']?>"><i class="fas fa-info-circle" style="margin-right: 10px;"></i>Hapus</button>
-                          </td>
+													<td><?= $data[1] ?></td>
+													<td><?= ($data[2] == 'pemasukan')? 'Tabungan Masuk':'Tabungan Keluar'?></td>
+													<td><?= rupiah($data[3])?></td>
+													<td><?= $data[4];?></td>
+													<td><?= $data[5];?></td>
+                                                    <td>
+                                                        <!-- <a style="color: white;" class="btn btn-success btn-sm" data-toggle="modal" data-target="#edit<?=$data['2']?>"><i class="fas fa-check-circle" style="margin-right: 10px;"></i>Accept</a> -->
+                                                        <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#acc<?= $data[0]?>"><i class="fas fa-check-circle" style="margin-right: 10px;"></i>Accept</button>
+                                                        <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#hapus<?=$data['1']?>"><i class="fas fa-times-circle" style="margin-right: 10px;"></i>Reject</button>
+                                                    </td>
 												</tr>
 
-                        <!-- Modal Hapus -->
-                        <div class="modal fade" id="hapus<?=$data['2']?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                          <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Hapus Data</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                                </button>
-                              </div>
-                              <input type="text" name="id" value="<?= $data['2']?>" readonly hidden>
-                              <div class="modal-body">
-                                Apakah anda yakin menghapus data ?
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                                <form action="" method="POST">
-                                  <input type="text" name="id" value="<?=$data['2']?>" readonly hidden>
-                                  <input type="text" name="id_user" value="<?=$_SESSION['users']->id_user?>" readonly hidden>
-                                  <button type="submit" name="hapus" class="btn btn-danger">Hapus</button>
-                                </form>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                                                <!-- Modal Reject -->
+                                                <div class="modal fade" id="hapus<?=$data['1']?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel"><?= $judul?></h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <input type="text" name="id" value="<?= $data['2']?>" readonly hidden>
+                                                    <div class="modal-body">
+                                                        Apakah anda yakin mereject data ?
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                                        <form action="" method="POST">
+                                                        <input type="text" name="id" value="<?=$data['2']?>" readonly hidden>
+                                                        <input type="text" name="id_user" value="<?=$_SESSION['users']->id_user?>" readonly hidden>
+                                                        <button type="submit" name="reject" class="btn btn-danger">Reject</button>
+                                                        </form>
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                                </div>
 
-                        <!-- Modal Edit Data -->
-                        <div class="modal fade" id="edit<?=$data['2']?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                          <div class="modal-dialog modal-dialog-centered" role="document">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">Edit <?= $judul?></h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                                </button>
-                              </div>
-                              <div class="modal-body">
-                                <form method="POST" action="">
-                                    <div class="form-group">
-                                        <label for="formGroupExampleInput2">Nama Nasabah</label>
-                                        <input type="text" class="form-control" name="nama" value="<?= $data['3']?>" id="formGroupExampleInput2" placeholder="Nama Nasabah" required readonly>
-                                        <input type="text" class="form-control" name="id_user" value="<?= $data['1']?>" id="formGroupExampleInput2" required readonly hidden>
-                                        <input type="text" class="form-control" name="id" value="<?= $data['2']?>" id="formGroupExampleInput2" placeholder="Nama Nasabah" required readonly hidden>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="formGroupExampleInput2">Nominal</label>
-                                        <input type="number" class="form-control" name="nominal" value="<?= $data['5']?>" id="formGroupExampleInput2" placeholder="Nominal" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="formGroupExampleInput2">Keterangan</label>
-                                        <textarea name="ket" class="form-control" cols="30" rows="4" placeholder="Keterangan" required><?= $data['6']?></textarea>
-                                        <!-- <input type="text" class="form-control" name="ket" placeholder="Keterangan" requireq> -->
-                                    </div>
-                                
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="submit" name="edit" class="btn btn-primary">Simpan</button>
-                              </div>
-                              </form>
-                            </div>
-                          </div>
-                        </div>
-                        <!-- end modal -->
+                                                <!-- Modal Acc -->
+                                                <div class="modal fade" id="acc<?= $data[0]?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel"><?= $judul?></h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <input type="text" name="id_user" value="<?= $data?>" readonly hidden>
+                                                    <div class="modal-body">
+                                                        Apakah anda yakin mengaprove data ?
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                                        <form action="" method="POST">
+                                                        <input type="text" name="id_transaksi" value="<?=$data['0']?>" readonly hidden>
+                                                        <button type="submit" name="acc" class="btn btn-success">Acc</button>
+                                                        </form>
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                                </div>
 
-                        <?php } ?>
+                                                <?php } ?>
 											</tbody>
 										</table>
 									</div>
@@ -173,45 +157,5 @@ if (isset($_POST['edit'])) {
 				</div>
 			</div>
 		</div>
-
-
-<!-- Modal Tambah Data -->
-<div class="modal fade" id="tambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Tambah <?= $judul?></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form method="POST" action="">
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Nama Nasabah</label>
-                <input type="text" class="form-control" name="nama" id="formGroupExampleInput2" placeholder="Nama Nasabah" required>
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Nominal</label>
-                <input type="number" class="form-control" name="nominal" id="formGroupExampleInput2" placeholder="Nominal" required>
-            </div>
-            <div class="form-group">
-                <label for="formGroupExampleInput2">Keterangan</label>
-                <textarea name="ket" class="form-control" cols="30" rows="4" placeholder="Keterangan" required></textarea>
-                <!-- <input type="text" class="form-control" name="ket" placeholder="Keterangan" requireq> -->
-            </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" name="tambah" class="btn btn-primary">Save changes</button>
-      </div>
-      </form>
-    </div>
-  </div>
-</div>
-<!-- end modal -->
-
-
-
 
 <?php include(__DIR__.'/../template/footer.php'); ?>
